74042a6f2b270167c46de7721b4196f503876408
Jason Evans <evans.jason@hotmail.co.uk>
2026-08-10 13:44:49 +0100

    removed some components that may not be used in phase 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import AboutLeague from "../components/AboutLeague/AboutLeague";
-import ChampionsGallery from "../components/ChampionsGallery/ChampionsGallery";
 import CurrentSeason from "../components/CurrentSeason/CurrentSeason";
 import Hero from "../components/Hero/Hero";
 import HowItWorks from "../components/HowItWorks/HowItWorks";
 import JoinToday from "../components/JoinToday/JoinToday";
-import LeagueCentre from "../components/LeagueCentre/LeagueCentre";
 
 export default function Home() {
   return (
@@ -13,8 +11,6 @@ export default function Home() {
       <AboutLeague />
       <HowItWorks />
       <CurrentSeason />
-      <LeagueCentre />
-      <ChampionsGallery />
       <JoinToday />
     </>
   );
